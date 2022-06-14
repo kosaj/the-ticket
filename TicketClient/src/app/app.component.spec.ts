488829c2c4e7b75cbd04276c0f2594d4f7d0f8d8
kosaj <kosaj@users.noreply.github.com>
2022-06-14 23:51:04 +0200

    .

--- TicketClient/src/app/app.component.spec.ts
+++ TicketClient/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TicketApp'`, () => {
+  it(`should have as title 'TicketClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TicketApp');
+    expect(app.title).toEqual('TicketClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TicketApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TicketClient app is running!');
   });
 });
