d458a82afde6cce5467393e41fe5536709515d25
kosaj <kosaj@users.noreply.github.com>
2022-06-15 01:43:11 +0200

    .

--- TicketClient/src/app/app.component.ts
+++ TicketClient/src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { take } from "rxjs";
 
 @Component({
   selector: "app-root",
   template: `
     <h1>Welcome to {{ title }}!</h1>
+    <button (click)="ping()">SEND API PING</button>
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
   title = "TicketClient";
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  ping(): void {
+    this.httpClient.get("/api").pipe(take(1)).subscribe();
+  }
 }
