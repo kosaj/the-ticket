5513dec28d5202f85662bf25a4d25dcce08431f3
kosaj <kosaj@users.noreply.github.com>
2022-06-17 22:45:57 +0200

    .

--- TicketSender/src/main.ts
+++ TicketSender/src/main.ts
@@ -1,7 +1,11 @@
-import client, { Connection } from "amqplib";
+import { Connection } from "amqplib";
 
-const connection: Connection = await client.connect(
-  "amqp://username:password@localhost:5672"
-);
+// const connection: Connection = await client.connect(
+//   "amqp://username:password@localhost:5672"
+// );
+
+export class Test {
+  test!: Connection;
+}
 
 console.log("HELLO WORLD!");
