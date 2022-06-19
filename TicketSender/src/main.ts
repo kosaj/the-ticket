88a50e8ddd488694f72f2bdf546c3d3754a01bfc
kosaj <kosaj@users.noreply.github.com>
2022-06-19 11:57:21 +0200

    .

--- TicketSender/src/main.ts
+++ TicketSender/src/main.ts
@@ -1,22 +1,8 @@
 import { Connection } from "amqplib";
+export class App {
+  test: Connection | null = null;
 
-// const connection: Connection = await client.connect(
-//   "amqp://username:password@localhost:5672"
-// );
-
-class App {
-  test!: Connection;
-
-  async init(): Promise<void> {
-    let result = new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
-
-    await result;
+  start(): void {
+    console.log();
   }
 }
-
-const current = Date.now();
-console.log("BEGIN");
-await new App().init();
-console.log("END", (Date.now() - current) / 1000);
